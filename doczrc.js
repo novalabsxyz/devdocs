d7e0900832232e5d2dcea236e766b2b53d5ac77c
Kent Williams <k3nt00@gmail.com>
2019-09-17 10:12:55 -0700

    updated doc structure and menu

--- doczrc.js
+++ doczrc.js
@@ -1,4 +1,30 @@
+// export default {
+//   files: '**/*.{mdx}',
+//   theme: 'theme/index'
+// }
+
 export default {
-  files: '**/*.{mdx}',
-  theme: 'theme/index'
+  title: 'Helium Developers',
+  theme: 'theme/index',
+  menu: [
+    {
+      name: 'Home',
+    },
+    {
+      name: 'Device',
+      menu: [
+        'LongFi Protocol',
+        'LongFi SDK',
+        'HeliOS',
+      ],
+    },
+    {
+      name: 'Blockchain',
+      menu: [
+        'API',
+      ],
+    },
+  ],
+  ignore: ['README.md'],
 }
+
