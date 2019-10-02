bdde7f35d2fa9d0c31319361466ccbb9752eaf0a
Kent Williams <k3nt00@gmail.com>
2019-10-02 13:29:09 -0700

    added gatsby-theme-docz theme styling source

--- doczrc.js
+++ doczrc.js
@@ -6,5 +6,14 @@ export default {
       body: 'Notable, sans-serif',
       heading: "Notable, sans-serif", 
     },
+    styles: {
+      Container: {
+        p: 4,
+        maxWidth: 1280,
+      },
+      h1: {
+        fontSize: 6,
+      },    
+    },
   },
 }
