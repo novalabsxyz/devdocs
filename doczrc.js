6bf441b19216db3dca2bc716d87aa1b0e5ec9cdc
Kent Williams <k3nt00@gmail.com>
2019-10-02 13:02:06 -0700

    imported new font and set in themeconfig

--- doczrc.js
+++ doczrc.js
@@ -1,20 +1,10 @@
 // doczrc.js
 export default {
   ignore: ['README.md'],
-  menu: [
-    {
-      name: 'Device',
-      menu: [
-        'LongFi Protocol',
-        'LongFi SDK',
-        'HeliOS',
-      ],
+  themeConfig: {
+    fonts: {
+      body: 'Notable, sans-serif',
+      heading: "Notable, sans-serif", 
     },
-    {
-      name: 'Blockchain',
-      menu: [
-        'API',
-      ],
-    },
-  ],
+  },
 }
