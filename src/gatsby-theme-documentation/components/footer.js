efd10c4207fc3b1c127bcdd82966c0b298a48c38
Kent Williams <k3nt00@gmail.com>
2019-10-04 10:55:29 -0700

    made footer thinner

--- src/gatsby-theme-documentation/components/footer.js
+++ src/gatsby-theme-documentation/components/footer.js
@@ -8,36 +8,23 @@ function Footer() {
     return (
         <footer
             sx={{
-                fontSize: 2,
+                display: 'flex',
+                flexWrap: 'wrap',
+                alignItems: 'center',
+                p: 2,
                 variant: 'styles.footer',
             }}>
-            <div
-                sx={{
-                    display: 'grid',
-                    gridTemplateRows: 'repeat(4, 32px)',
-                    gridTemplateColumns: [
-                        'repeat(2, 1fr)',
-                        'repeat(4, 1fr)',
-                    ],
-                    gridAutoFlow: 'column',
-                    px: 2,
-                    py: 4,
-                }}>
-                <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Home</Link>
-                <Link to='/device' sx={{ variant: 'styles.navlink', p: 2 }}>Device</Link>
-            </div>
-            <div
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'flex-end',
-                    p: 3,
-                }}>
-                <Link to='/' sx={{ color: 'inherit' }}>Privacy Policy</Link>
-                <div sx={{ mx: 1 }} />
-                <Link to='/' sx={{ color: 'inherit' }}>Terms of Use</Link>
-                <div sx={{ mx: 1 }} />
-                © 2019 Jane Doe
-            </div>
+            <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>
+                Introduction
+            </Link>
+            <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>
+                Blog
+            </Link>
+            <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>
+                About
+            </Link>
+            <div sx={{ mx: 'auto' }} />
+            <div sx={{ p: 2 }}>© 2019 Helium</div>
         </footer>
     )
 }
