3558afb7a14aebb3456c9f8bcfd297de2324613c
Kent Williams <k3nt00@gmail.com>
2019-10-08 11:01:38 -0700

    imported logo file directly

--- src/gatsby-theme-documentation/components/logo.js
+++ src/gatsby-theme-documentation/components/logo.js
@@ -1,7 +1,9 @@
 // src/gatsby-theme-dox/components/Logo.js
 import { Link } from 'gatsby'
 import React from 'react'
+import logo_image from "../../static/logo.svg"
+
 function Logo(props) {
-  return <Link to='/' title='Home'><img src="/static/logo.svg" alt="homepage" {...props} /> </Link>
+  return <Link to='/' title='Home'><img src={logo_image} alt="homepage" {...props} /> </Link>
 }
 export default Logo
